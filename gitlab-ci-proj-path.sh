c1c2a7319419957683e0d38d9dbdd59625d51853
Aleksandr Kelbas <kelbas.aleksandr@gmail.com>
2016-12-14 08:54:40 +0000

    Update gitlab-ci-proj-path.sh

--- gitlab-ci-proj-path.sh
+++ gitlab-ci-proj-path.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 # Please make sure you add GITLAB_DOMAIN and HTML_COV_FILE_NAME variable to your gitlab project configuration
+echo "Setting PROJ_PATH..."
+
+if [ $GITLAB_DOMAIN == "" ]; then
+  echo "GITLAB_DOMAIN environment variable not set"
+  exit 1
+fi
+
+if [ $CI_PROJECT_PATH == "" ]; then
+  echo "CI_PROJECT_PATH environment variable not set"
+  exit 1
+fi
+
+if [ $HTML_COV_FILE_NAME == ""]; then
+  echo "HTML_COV_FILE_NAME environment variable not set"
+  exit 1
+fi
+
 export PROJ_PATH="$GOPATH/src/$GITLAB_DOMAIN/$CI_PROJECT_PATH"
+echo "PROJ_PATH: $PROJ_PATH"
 export HTML_COV_FILE_PATH="$PROJ_PATH/$HTML_COV_FILE_NAME"
+echo "HTML_COV_FILE_PATH: $HTML_COV_FILE_PATH"
 rm -rf $PROJ_PATH
 mkdir -p $PROJ_PATH
 cp -r . $PROJ_PATH
 cd $PROJ_PATH
+echo "Done."
